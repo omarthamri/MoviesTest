070a35fea6ec7eaa464f3f541a3a5adfa7976c51
omarthamri <omarthamri4@gmail.com>
2024-05-20 15:19:34 +0100

    Initial Commit

---
+++ MoviesTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesTest
+//
+//  Created by omar thamri on 20/5/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
